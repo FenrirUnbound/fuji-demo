a6962f806938cc2aa230c60255e9b231b331eb01
Darren M <bobafett@yahoo-inc.com>
2017-06-07 11:21:11 -0700

    feat: deploy job uses packer image

---
+++ config/install_packer.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget https://releases.hashicorp.com/packer/1.0.0/packer_1.0.0_linux_amd64.zip
+apt-get -y install unzip
+unzip packer_1.0.0_linux_amd64.zip
+mv packer /usr/local/bin
+packer version
